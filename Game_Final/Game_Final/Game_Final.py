e98396c5ff7a4accf489e9cfdccddce67d3e47bd
MiniMidgetMichael <child2dad@gmail.com>
2016-05-31 10:52:29 -0700

    Created tron class

--- Game_Final/Game_Final/Game_Final.py
+++ Game_Final/Game_Final/Game_Final.py
@@ -1,7 +1,19 @@
 ﻿import turtle
 
-blue = turtle.Turtle()
-orange = turtle.Turtle()
 screen = turtle.Screen()
-blue.ht()
-orange.ht()
+class tron(object):
+    def __init__(self, color):
+        assert type(color) is str,"color must be a string, not %s" % type(color)
+        self.color = color
+        Turtle = turtle.Turtle()
+        self.turtle = Turtle
+        Turtle.hideturtle()
+        try:
+            Turtle.color(color)
+        except turtle.TurtleGraphicsError as e:
+            print (e)
+            return
+            
+blue = tron("blue")
+orange = tron("orange")
+#test = tron("test")
