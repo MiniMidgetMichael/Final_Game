2253bf1534d3d2d3f156a09c9c60c4dbdacabfc5
MiniMidgetMichael <child2dad@gmail.com>
2016-06-03 12:02:13 -0700

    Worked on cooldown

--- Game_Final/Game_Final/Game_Final.py
+++ Game_Final/Game_Final/Game_Final.py
@@ -14,6 +14,12 @@ class tron(object):
         self.Turtle = Turtle
         Turtle.ht()
         Turtle.pu()
+        turned = False
+        self.turned = turned
+        clk = 0
+        self.clk = clk
+        cooldown = 0.0
+        self.cooldown = cooldown
         ## hides turtle ('ht' = 'hideturtle')
 
         try:
@@ -33,39 +39,85 @@ class tron(object):
         assert type(ctrls) is dict, "ctrls must be dict, {'key' : fun,  ...} not %s" % type(ctrls)
         for k, f in ctrls.items():
             screen.onkeypress(f, k)
+    
+    def turn_left(self):
+        turned = self.turned
+        cd = self.cooldown
+        clk = self.clk
+        Turtle = self.Turtle
+
+        if cd == 0.0:
+            Turtle.lt(90)
+            turned = True
+        else:
+            return
+        self.turned = turned
+
+
+    def turn_right(self):
+        turned = self.turned
+        cd = self.cooldown
+        clk = self.clk
+        Turtle = self.Turtle
+
+        if cd == 0.0:
+            Turtle.rt(90)
+            turned = True
+        else:
+            return
+        self.turned = turned
+            
 
     def cycle(self):
+        turned = self.turned
         is_AI = self.is_AI
         screen = self.screen
+        clk = self.clk
+        cd = self.cooldown
         Turtle = self.Turtle
         player = self.player
-        if player == 1 and Turtle.xcor() == -20:
-            print ("Player 1, you died!")
-            screen.bye()
-            return
-        #elif player == 2 and Turtle.xcor() == 20:
-        #    print ("Player 2, you died!")
-        #    screen.bye()
-        #    return
-        Turtle.fd(2)
-        time.sleep(0.0125)
-        print (Turtle.pos())
+        x_pos = Turtle.xcor()
+        y_pos = Turtle.ycor()
+        print ("turned = ", turned)
+        if turned == True:
+            cd += 0.1
+            print ("cd = ", cd)
+
+        if cd >= 1.0:
+            print ("#cd >= 1")
+            cd = 0.0
+            turned = False
+
+        self.cooldown = cd
+        self.turned = turned
+        if (abs(x_pos) == screen.window_width()/2) or (abs(y_pos) == screen.window_height()/2):
+            print ("Player %s, You Died!" % player)
+            return 0
+        Turtle.fd(3)
+        
+        #print (Turtle.pos())
+            
+            
+
+"""
+    END OF CLASS
+"""
 
 def main_menu(scr):
     assert type(scr) is turtle._Screen,"scr must be turtle.Screen, not %s" % type(scr)
     players = screen.numinput("Number of players", "How many players? (1 or 2)", minval = 1, maxval = 2)
     if players == 1:
-        p1_color = functions.good_input("What color do you want your bike to be?: \n", values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'])
+        p1_color = functions.turtle_good_input("What color do you want your bike to be?: \n", scr, values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
         p1 = tron(p1_color, scr, 1)
-        p2_color = functions.good_input("What color do you want your enemy's bike to be?: \n", values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'])
+        p2_color = functions.turtle_good_input("What color do you want your enemy's bike to be?: \n", scr, values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
         p2 = tron(p2_color, scr, 2, is_AI = True)
         
         p1.Turtle.setx(-200)
         p2.Turtle.setx(200)
     else:
-        p1_color = functions.good_input("Player 1: What color do you want your bike to be?: \n", values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'])
+        p1_color = functions.turtle_good_input("Player 1: What color do you want your bike to be?: \n", scr, values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
         p1 = tron(p1_color, scr, 1)
-        p2_color = functions.good_input("Player 2: What color do you want your bike to be?: \n", values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'])
+        p2_color = functions.turtle_good_input("Player 2: What color do you want your bike to be?: \n", scr, values = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
         p2 = tron(p2_color, scr, 2, is_AI = True)
         
         p1.Turtle.setx(-200)
@@ -73,19 +125,19 @@ def main_menu(scr):
     p2.Turtle.rt(180)
     return p1, p2, int(players)
 
-def play(p1, p2, num_players):
-    p1_ready = functions.good_input("Player 1: Are you ready? [y or n] : \n", values = ['y', 'n']).casefold()
+def play(p1, p2, num_players, screen):
+    p1_ready = functions.turtle_good_input("Player 1: Are you ready? [y or n] : \n", screen, values = ['y', 'n']).casefold()
     while not(p1_ready == 'y'):
         print ("Waiting...")
         time.sleep(1)
-        p1_ready = functions.good_input("Player 1: Are you ready? [y or n] : \n", values = ['y', 'n']).casefold()
+        p1_ready = functions.turtle_good_input("Player 1: Are you ready? [y or n] : \n", screen, values = ['y', 'n']).casefold()
     else:
         if num_players == 2:
-            p2_ready = functions.good_input("Player 2: Are you ready? [y or n] : \n", values = ['y', 'n']).casefold()
+            p2_ready = functions.turtle_good_input("Player 2: Are you ready? [y or n] : \n", screen, values = ['y', 'n']).casefold()
             while not(p2_ready == 'y'):
                 print ("Waiting...")
                 time.sleep(1)
-                p2_ready = functions.good_input("Player 2: Are you ready? [y or n] : \n", values = ['y', 'n']).casefold()
+                p2_ready = functions.turtle_good_input("Player 2: Are you ready? [y or n] : \n", screen, values = ['y', 'n']).casefold()
             all_ready = True
         else:
             all_ready = True
@@ -107,33 +159,32 @@ if __name__ == "__main__":
     screen.bgpic("P:/Python/Final_Game/Game_Final/Resources/Grid.gif")
     screen.setup(900, 900)
     #screen.listen()
-    def p1_left():
-        p1.Turtle.lt(90)
-    def p1_right():
-        p1.Turtle.rt(90)
-    def p2_left():
-        p2.Turtle.lt(90)
-    def p2_right():
-        p2.Turtle.rt(90)
+
 
     p1_ctrls = {
-        'a' : p1_left,
-        'd' : p1_right
+        'a' : p1.turn_left,
+        'd' : p1.turn_right,
+        'q' : screen.bye
     }
     p2_ctrls = {
-        'Left' : p2_left,
-        'Right' : p2_right
+        'Left' : p2.turn_left,
+        'Right' : p2.turn_right
     }
-    if players == 1:
-        p1.set_controls(p1_ctrls)
-    else:
-        p1.set_controls(p1_ctrls)
-        p2.set_controls(p2_ctrls)
+    screen.listen()
 
-    play(p1, p2, players)
 
+    play(p1, p2, players, screen)
     
+    #screen.onkeypress(screen.bye, 'q')
+    clk = p1.clk
     while screen._RUNNING:
-        p1.cycle()
-        p2.cycle()
+        if p1.cycle() == 0 or p2.cycle() == 0:
+            break
+        if players == 1:
+            p1.set_controls(p1_ctrls)
+        else:
+            p1.set_controls(p1_ctrls)
+            p2.set_controls(p2_ctrls)
+        time.sleep(0.00125)
+
     #sets screen size to 900x900 pixels
--- Game_Final/Game_Final/functions.py
+++ Game_Final/Game_Final/functions.py
@@ -1,6 +1,6 @@
-#! C:/Users/MichaelLFarwell/AppData/Local/Programs/Python/Python35-32/python.exe
+﻿#! C:/Users/MichaelLFarwell/AppData/Local/Programs/Python/Python35-32/python.exe
 from inspect import isclass
-import pickle, os
+import pickle, os, turtle
 
 
 
@@ -15,6 +15,32 @@ def good_input(prompt, typ=None, values=None):
         is_good = False
         while not(is_good):
             ans = input(prompt).casefold()
+            if not(typ is None):
+                try:
+                    typ(ans)
+                    is_good = True
+                    return ans
+                except:
+                    print ("%s is not a valid response" % ans)
+            elif not(values is None):
+                if ans in values:
+                    is_good = True
+                    return ans
+                else:
+                    print ("Please only answer with one of %s, not %s" % (values, ans))
+
+def turtle_good_input(prompt, screen, typ=None, values=None):
+        assert (type(prompt) is str),"prompt must be <class 'str'>, not %s" % type(prompt)
+        assert (not(typ is None) or not(values is None)),"must be one of either 'typ' or 'values'"
+        assert (type(screen) is turtle._Screen),"screen must be <class 'turtle._Screen'>, not %s" % type(screen)
+        if not(typ is None):
+            assert (isclass(typ)),"typ must be a class type"
+        if not(values is None):
+            assert (type(values) is list),"values must be <class 'list'>, not %s" % type(values)
+
+        is_good = False
+        while not(is_good):
+            ans = screen.textinput(prompt, prompt).casefold()
             if not(typ is None):
                 try:
                     typ(ans)
